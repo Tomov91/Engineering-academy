30de9933a9d1d6e3c78c21698e2938a8835941b2
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-05-13 13:42:55 +0300

    Add files via upload

---
+++ 1.C and C++ development_basics-Homework/Task 1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+/*Problem 1: 1.	Write a program that inputs a positive integer N and outputs the factorial of 2N*/
+/*Thus, 2n!=2n.2n−1.2n−2........3.2.1*/
+/*Im not sure */
+int main()
+{
+	int n; int sum=1;
+
+    std::cin >> n;
+
+	for (size_t i = 1; i < n; i++)
+	{
+		sum = sum * (i + 1);
+	}
+
+	std::cout << 2*sum;
+	std::cout << 
+}
+
---
+++ 1.C and C++ development_basics-Homework/Task 2.cpp
@@ -0,0 +1,22 @@
+﻿#include<string>
+#include <iostream>
+/*2.	Write a program that counts and outputs the number of digits in Nth Fibonacci number 
+•	Example:
+Please input a positive integer: 13
+Number of digits in F5 is: 3
+*/
+int main()
+{
+    std::string digits;
+    int n; int next = 0; int curr = 1; int sum = 0;
+    std::cout << "Please input a positive integer: " << std::endl; std::cin >> n;
+    
+    for (size_t i = 0; i < n; i++)
+    {
+        sum = curr + next;
+        next = curr;
+        curr = sum;
+    }
+    digits = std::to_string(sum);
+    std::cout << digits.length();
+}
