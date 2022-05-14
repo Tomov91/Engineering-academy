01169039d9c740ab720f1a2c88c6efeb77922124
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-05-14 20:36:20 +0300

    Add files via upload

---
+++ 1.C and C++ development_basics-Homework/Task 3.cpp
@@ -0,0 +1,18 @@
+﻿/*3.	Write a program that inputs three real numbers and outputs whether they can be sides of a triangle*/
+
+#include <iostream>
+
+int main()
+{
+    int a, b, c; 
+    std::cout << "Input sides of triangle: " << std::endl;
+    std::cin >> a >> b >> c;
+    if (a + b > c and a + c > b and b + c > a) {
+        std::cout << "It's a valid triangle" << std::endl;
+    }
+    else
+    {
+        std::cout << "It can not be valid triangle" << std::endl;
+    }
+}
+
