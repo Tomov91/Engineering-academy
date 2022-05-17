c668c616d83e0305405a200fbf61e0117f119041
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-05-17 19:08:12 +0300

    Add files via upload

---
+++ 1.C and C++ development_basics-Homework/Task 4.cpp
@@ -0,0 +1,20 @@
+﻿/*4.	Write a program that calculates and outputs the first N even Fibonacci numbers, separated by comma and space.
+        N is entered from the standard input.*/
+#include <iostream>
+
+int main()
+{
+	char const*sep = "";
+    int n; int sum = 0; int curr = 1; int next = 0;
+    std::cin >> n;
+	for (size_t i = 0; i < n; i++)
+	{
+		sum = curr + next;
+		next = curr;
+		curr = sum;
+		if (sum % 2 == 0) {
+			std::cout << sep << sum ;
+			sep = ", ";
+		}
+	}
+}
