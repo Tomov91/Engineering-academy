23b9c7d66206163d4d9fe0bb0bad1a57c7082a3b
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-05-19 22:55:36 +0300

    Add files via upload

---
+++ 1.C and C++ development_basics-Homework/Task 8.cpp
@@ -0,0 +1,27 @@
+﻿/*8.	The same triangle as previous, but having height and symbol input by the user. */
+#include <iostream>
+
+int main()
+{
+    std::cout << "Choose height: ";
+    int height;
+    std::cin >> height;
+    int size = height * 2;
+    char c;
+    std::cout << "Choose symbol: ";
+    std::cin >> c;
+    for (size_t i = 0; i < height; i++)
+    {
+        int k = i;
+        while (k--)
+        {
+            std::cout << " ";
+        }
+        size -= 2;
+        for (size_t j = 0; j <= size; j++)
+        {
+            std::cout << c;
+        }
+        std::cout << std::endl;
+    }
+}
---
+++ 1.C and C++ development_basics-Homework/Task 9.cpp
@@ -0,0 +1,41 @@
+﻿/*9.	Write a program that outputs the first N natural numbers in a rectangle with a given width (W).
+        The width is the number of symbols (not numbers) in a line!
+        Numbers are printed in ascending order sequentially in horizontal direction until line is complete and the sequence continues on the next line,
+        etc. No spaces. The text on the console shall look as a rectangular block (the last line may be incomplete).
+        N and W are entered by the user.*/
+
+#include <iostream>
+#include<string>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+
+	int n;
+	int w;
+	cout << "Input numbers: " << endl;
+	cin >> n;
+	cout << "Input width: " << endl;
+	cin >> w;
+	string wStr = to_string(w);
+	string var;
+	int counter = 0;
+	for (size_t i = 0; i <= n; i++)
+	{
+		var = to_string(i);
+		for (size_t j = 0; j < var.size(); j++)
+		{
+			counter++;
+			cout << var[j];
+			if (counter == w) {
+				cout << endl;
+				counter = 0;
+			}
+		}
+	}
+}
+
+
+
