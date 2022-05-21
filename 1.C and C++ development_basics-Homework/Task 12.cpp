57e1eb04de74add0a6e51fccfca96cdbddf56639
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-05-21 20:59:25 +0300

    Add files via upload

---
+++ 1.C and C++ development_basics-Homework/Task 12.cpp
@@ -0,0 +1,25 @@
+﻿/*12.	Write a program that prints the maximum number in a hailstone sequence,
+        given the seed N. The seed is entered from the standard input. */
+
+#include <iostream>
+#include<vector>
+#include<algorithm>
+
+int main()
+{
+	std::vector<int> numbers;
+    int n; std::cin >> n;
+	while (n!=1)
+	{
+		if (n % 2 == 0) {
+			n = n / 2;
+		}
+		else
+		{
+			n = (3 * n + 1);
+		}
+		numbers.push_back(n);
+	}
+	std::cout << *max_element(numbers.begin(), numbers.end());
+}
+
---
+++ 1.C and C++ development_basics-Homework/Task 13.cpp
@@ -0,0 +1,26 @@
+﻿/*13.	Write a program that is capable of inputting very long (i.e. 40-digit) numbers
+        from the standard input and checks whether the number is a palindrome. 
+        The program shall write “Palindromic” or “Not palindromic” to the standard output. */
+
+#include <iostream>
+#include<string>
+std::string isPalindrome(std::string& number)
+{
+    // Iterate over the range [0, N/2]
+    for (int i = 0; i < number.length() / 2; i++) {
+        if (number[i] != number[number.length() - i - 1]) {
+            return "Not Palindromic";
+        }
+    }
+    return "Palindromic";
+}
+
+int main()
+{
+    std::string number;
+    std::cin >> number;
+    std::cout << isPalindrome(number);
+
+    return 0;
+}
+
