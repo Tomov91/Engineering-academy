1cf337df9b5fceb60d70ac9c3e543b6ac86e973b
Tomov91 <97407411+Tomov91@users.noreply.github.com>
2022-05-25 18:56:52 +0300

    Add files via upload

---
+++ 1.C and C++ development_basics-Homework/Task 17.cpp
@@ -0,0 +1,35 @@
+﻿#include<vector>
+#include<map>
+#include<string>
+#include<sstream>
+#include <iostream>
+#include<cctype>
+#include<set>
+using namespace std;
+
+void read(string s) {
+	char word;
+	istringstream str(s);
+
+	map<char, int> howmany;
+
+	while (str >> word)
+	{
+		howmany[tolower(word)]++;
+
+	}
+	map <char, int > ::iterator it;
+	for (it = howmany.begin(); it != howmany.end(); it++) {
+		cout << it->first << " - > " << it->second << endl;
+	}
+}
+
+int main()
+{
+	string sentence = "Free your mind and the rest will follow";
+	read(sentence);
+}
+
+
+
+
